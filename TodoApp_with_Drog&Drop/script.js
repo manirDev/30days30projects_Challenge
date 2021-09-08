0f0a5914478ccee4e5e057c195ea5ac3a9a80f9d
Mahamat Nour Mahamat Abdraman <42040735+manirDev@users.noreply.github.com>
2021-09-08 10:39:35 +0300

    TodoApp_with_Drog&Drop

---
+++ TodoApp_with_Drog&Drop/script.js
@@ -0,0 +1,50 @@
+
+const dragStart = target => {
+    target.classList.add('dragging');
+};
+
+const dragEnd = target => {
+    target.classList.remove('dragging');
+};
+
+const dragEnter = event => {
+    event.currentTarget.classList.add('drop');
+};
+
+const dragLeave = event => {
+    event.currentTarget.classList.remove('drop');
+};
+
+const drag = event => {
+    event.dataTransfer.setData('text/html', event.currentTarget.outerHTML);
+    event.dataTransfer.setData('text/plain', event.currentTarget.dataset.id);
+};
+
+const drop = event => {
+    document.querySelectorAll('.col').forEach(col => col.classList.remove('drop'));
+    document.querySelector(`[data-id="${event.dataTransfer.getData('text/plain')}"]`).remove();
+
+    event.preventDefault();
+    event.currentTarget.innerHTML = event.currentTarget.innerHTML + event.dataTransfer.getData('text/html');
+};
+
+const allowDrop = event => {
+    event.preventDefault();
+};
+
+document.querySelectorAll('.col').forEach(col => {
+    col.addEventListener('dragenter', dragEnter);
+    col.addEventListener('dragleave', dragLeave);
+});
+
+document.addEventListener('dragstart', e => {
+    if (e.target.className.includes('panel')) {
+        dragStart(e.target);
+    }
+});
+
+document.addEventListener('dragend', e => {
+    if (e.target.className.includes('panel')) {
+        dragEnd(e.target);
+    }
+});
