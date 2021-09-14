bda85aa1bfb4e327b13223653b6d738e894b3d96
Mahamat Nour Mahamat Abdraman <42040735+manirDev@users.noreply.github.com>
2021-09-14 19:52:11 +0300

    Add files via upload

---
+++ List_Expand_Collapse/script.js
@@ -0,0 +1,8 @@
+const toggles = document.querySelectorAll('.toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', ()=>{
+        toggle.parentNode.classList.toggle('active')
+        
+    })
+})
